4b7662a42d1e77396b3029e3351d5efb95729599
Jeffmelon <megamanethan@gmail.com>
2023-06-13 15:58:23 -0600

    nv

--- web-port/src/Components/Navbar.js
+++ web-port/src/Components/Navbar.js
@@ -1,8 +1,18 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 function Navbar() {
   return (
-    <div>Navbar</div>
+    <div className='navBar'>
+        <div className='toggle'>
+            <button></button>
+        </div>
+        <div className='links'>
+            <link to="/"> Home </link>
+            <link to="/websites"> Websites </link>
+            <link to="/art"> Home </link>
+        </div>
+    </div>
   )
 }
 

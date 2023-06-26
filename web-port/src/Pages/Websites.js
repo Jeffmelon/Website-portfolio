c6df06fd6fab37f1c9cdf3c692f7257b9fc4ee70
Jeffmelon <megamanethan@gmail.com>
2023-06-26 13:17:09 -0600

    helper

--- web-port/src/Pages/Websites.js
+++ web-port/src/Pages/Websites.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import WebList from '../Components/WebList';
+import HTMLCSS from '../assets/HTMLCSS.png';
+import Movies from '../assets/Movies.png';
+import TODO from '../assets/TODO.png';
+import "../Style/website.css";
 
 function Websites() {
   return (
     <div className='websites'>
       <h1>My Websites</h1>
       <div className='webList'>
-        <WebList name="HTML/CSS pratice Site"/>
-        <WebList name="To Do List Site"/>
-        <WebList name="Movie API Site"/>
+        <WebList name="HTML/CSS pratice Site" image={HTMLCSS}/>
+        <WebList name="To Do List Site" image={Movies}/>
+        <WebList name="Movie API Site" image={TODO}/>
         <WebList name="Boardgame Angular Site"/>
         <WebList name="HR Timesheet Angular Site"/>
       </div>

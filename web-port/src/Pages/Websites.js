713f0d799fa2702a5f7290ac0e13856d5d6c0379
Jeffmelon <megamanethan@gmail.com>
2023-06-29 11:14:41 -0600

    weblist

--- web-port/src/Pages/Websites.js
+++ web-port/src/Pages/Websites.js
@@ -4,6 +4,7 @@ import HTMLCSS from '../assets/HTMLCSS.png';
 import Movies from '../assets/Movies.png';
 import TODO from '../assets/TODO.png';
 import "../Style/website.css";
+import {webList} from "../helpers/WebList.js"
 
 function Websites() {
   return (
@@ -11,8 +12,8 @@ function Websites() {
       <h1>My Websites</h1>
       <div className='webList'>
         <WebList name="HTML/CSS pratice Site" image={HTMLCSS}/>
-        <WebList name="To Do List Site" image={Movies}/>
-        <WebList name="Movie API Site" image={TODO}/>
+        <WebList name="To Do List Site" image={TODO}/>
+        <WebList name="Movie API Site" image={Movies}/>
         <WebList name="Boardgame Angular Site"/>
         <WebList name="HR Timesheet Angular Site"/>
       </div>

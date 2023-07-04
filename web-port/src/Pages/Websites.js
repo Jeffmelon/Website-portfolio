3cab21de7cb41deb26b37bfcebddfd9a627ae410
Jeffmelon <megamanethan@gmail.com>
2023-07-04 12:00:13 -0600

    web maker

--- web-port/src/Pages/Websites.js
+++ web-port/src/Pages/Websites.js
@@ -1,21 +1,16 @@
 import React from 'react';
 import WebList from '../Components/WebList';
-import HTMLCSS from '../assets/HTMLCSS.png';
-import Movies from '../assets/Movies.png';
-import TODO from '../assets/TODO.png';
 import "../Style/website.css";
 import {webList} from "../helpers/WebList.js"
 
 function Websites() {
   return (
     <div className='websites'>
-      <h1>My Websites</h1>
+      <h1 className='webTitle'>My Websites</h1>
       <div className='webList'>
-        <WebList name="HTML/CSS pratice Site" image={HTMLCSS}/>
-        <WebList name="To Do List Site" image={TODO}/>
-        <WebList name="Movie API Site" image={Movies}/>
-        <WebList name="Boardgame Angular Site"/>
-        <WebList name="HR Timesheet Angular Site"/>
+        {webList.map((website) => {
+          return <WebList name={website.name} image={website.image}/>
+        })}
       </div>
     </div>
   )
